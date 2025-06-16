a6d0e1ed2a951720d8414e561170e5d462522598
alm0r <126470159+alm0r@users.noreply.github.com>
2025-06-16 14:16:13 +0200

    Are you playing banjo solution

---
+++ JS/Kyu 8/Strings/js/Are You Playing Banjo.js
@@ -0,0 +1,40 @@
+///////////////////////////////////////////////////////////////////////////////////
+// Create a function which answers the question "Are you playing banjo?".
+// If your name starts with the letter "R" or lower case "r", you are playing banjo!
+// The function takes a name as its only argument, and returns one of the following strings:
+
+// name + " plays banjo"
+// name + " does not play banjo"
+
+///////////////////////////////////////////////////////////////////////////////////
+
+// Method 1
+function areYouPlayingBanjo(name) {
+  if (name.charAt(0) === "R" || name.charAt(0) === "r") {
+    return name + " plays banjo";
+  } else {
+    return name + " does not play banjo";
+  }
+}
+
+//Cleaner
+function areYouPlayingBanjo(name) {
+  if (name[0].toLowerCase() === "r") {
+    return name + " plays banjo";
+  } else {
+    return name + " does not play banjo";
+  }
+}
+
+//Ternary
+function areYouPlayingBanjo(name) {
+  return name + (name[0].toLowerCase() == "r" ? " plays" : " does not play") + " banjo";
+}
+
+//Arrow + Ternary
+const areYouPlayingBanjo = (name) => {
+  const plays = name.toLowerCase().startsWith("r") ? "plays" : "does not play";
+  return `${name} ${plays} banjo`;
+};
+
+// ___________________________________________________________________________________;
