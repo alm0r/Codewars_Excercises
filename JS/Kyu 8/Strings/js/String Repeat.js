47650c97813f2aef653f69f1c191de6dde3718e2
alm0r <126470159+alm0r@users.noreply.github.com>
2025-06-18 18:13:18 +0200

    String repeat solution (JS/Kyu8/Strings)

---
+++ JS/Kyu 8/Strings/js/String Repeat.js
@@ -0,0 +1,27 @@
+///////////////////////////////////////////////////////////////////////////////////
+// Write a function that accepts a non-negative integer n and a string s as parameters, and returns a string of s repeated exactly n times.
+///////////////////////////////////////////////////////////////////////////////////
+// Examples (input -> output)
+// 6, "I"     -> "IIIIII"
+// 5, "Hello" -> "HelloHelloHelloHelloHello"
+
+///////////////////////////////////////////////////////////////////////////////////
+
+//Method
+function repeatStr(n, s) {
+  return s.repeat(n);
+}
+
+// Arrow f
+const repeatStr = (n, s) => s.repeat(n);
+
+// For Loop
+function repeatStr(n, s) {
+  let newString = "";
+  for (let i = 0; i <= n; i++) {
+    newString += s;
+  }
+  return newString;
+}
+
+// _________________________________________________________;
