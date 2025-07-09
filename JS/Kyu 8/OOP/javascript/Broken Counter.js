9c69d3f18b2b90a326f8c9a8ab32920312815a14
alm0r <126470159+alm0r@users.noreply.github.com>
2025-07-09 13:01:55 +0200

    Broken Counter

---
+++ JS/Kyu 8/OOP/javascript/Broken Counter.js
@@ -0,0 +1,27 @@
+///////////////////////////////////////////////////////////////////////////////////
+// Broken Counter
+///////////////////////////////////////////////////////////////////////////////////
+
+// Our counter prototype is broken. Can you spot, what's wrong here?
+
+// ___________________________________________________________________;
+
+//
+
+function Counter() {
+  this.value = 0;
+}
+
+Counter.prototype.increase = function () {
+  this.value++;
+};
+
+Counter.prototype.getValue = function () {
+  return this.value;
+};
+
+Counter.prototype.reset = function () {
+  this.value = 0;
+};
+
+let caja = new Counter(10);
