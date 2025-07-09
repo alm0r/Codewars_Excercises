9c98c00d704c9f25dbd84c494bd1341a0fad3f57
alm0r <126470159+alm0r@users.noreply.github.com>
2025-07-09 13:05:09 +0200

    Broken Counter

--- JS/Kyu 8/OOP/javascript/Broken Counter.js
+++ JS/Kyu 8/OOP/javascript/Broken Counter.js
@@ -23,3 +23,55 @@ Counter.prototype.getValue = function () {
 Counter.prototype.reset = function () {
   this.value = 0;
 };
+
+// Class
+class Counter {
+  constructor() {
+    this.value = 0;
+  }
+
+  increase() {
+    this.value++;
+  }
+
+  getValue() {
+    return this.value;
+  }
+
+  reset() {
+    this.value = 0;
+  }
+}
+
+// Prototype
+function Counter() {
+  this.value = 0;
+}
+
+Counter.prototype = {
+  increase: function () {
+    this.value++;
+  },
+  getValue: function () {
+    return this.value;
+  },
+  reset: function () {
+    this.value = 0;
+  },
+};
+
+// Shorthand
+class Counter {
+  constructor() {
+    this.reset();
+  }
+  increase() {
+    this.value++;
+  }
+  getValue() {
+    return this.value;
+  }
+  reset() {
+    this.value = 0;
+  }
+}
