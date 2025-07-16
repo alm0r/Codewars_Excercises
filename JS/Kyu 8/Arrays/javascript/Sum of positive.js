f93d92eadd5862a66691d525e1d47f0f3f2f1998
alm0r <126470159+alm0r@users.noreply.github.com>
2025-07-16 10:57:26 +0200

    Sum of positive

---
+++ JS/Kyu 8/Arrays/javascript/Sum of positive.js
@@ -0,0 +1,26 @@
+///////////////////////////////////////////////////////////////////////////////////
+// Sum of positive
+///////////////////////////////////////////////////////////////////////////////////
+// You get an array of numbers, return the sum of all of the positives ones.
+
+// Example
+
+// [1, -4, 7, 12] => 1 + 7 + 12 = 20
+
+// Note
+// If there is nothing to sum, the sum is default to 0.
+// ___________________________________________________________________________________;
+
+// For Loop
+function positiveSum(arr) {
+  let finalNum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > 0) finalNum += arr[i];
+  }
+  return finalNum;
+}
+
+//  Method
+function positiveSum(arr) {
+  return arr.reduce((acc, c) => acc + (c > 0 ? c : 0), 0);
+}
